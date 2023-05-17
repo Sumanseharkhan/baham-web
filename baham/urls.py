78333d1cb9709b0858fa319320cd4537232a287a
Sumanseharkhan <48711570+Sumanseharkhan@users.noreply.github.com>
2023-05-17 23:29:06 +0500

    Update urls.py

--- baham/urls.py
+++ baham/urls.py
@@ -1,11 +1,14 @@
+from baham import views
 from django.urls import path
-from . import views
-
 
 urlpatterns = [
-    path('', views.view_home, name='home'),
-    path('baham/vehicles', views.view_vehicles, name='vehicles'),
-    path('baham/vehicles/create', views.create_vehicle, name='createvehicle'),
-    path('baham/vehicles/save/', views.save_vehicle, name='savevehicle'),
-    path('baham/aboutus', views.view_aboutus, name='aboutus'),
+    path('', views.view_index, name='home'),
+    path('baham/members/', views.view_members, name='members'),
+    path('baham/aboutus/', views.view_aboutus, name='aboutus'),
+    path('baham/vehicles/', views.view_vehicles, name='vehicles'),
+    path('baham/vehicles/create/', views.create_vehicle, name='create_vehicle'),
+    path('baham/vehicles/update/<id>', views.update_vehicle, name='update_vehicle'),
+    path('baham/vehicles/create/save/', views.save_vehicle, name='save_vehicle'),
+    path('baham/vehicles/void/<id>/', void_model, name='void_model'),
+    path('baham/vehicles/unvoid/<id>/', unvoid_model, name='unvoid_model'),
 ]
